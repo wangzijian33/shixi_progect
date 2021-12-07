f0687fd109739941609299d3ebbb3404052cadbf
yuzhaokui <1059816754@qq.com>
2021-12-07 09:43:43 +0800

    V1

--- a_msg.m
+++ a_msg.m
@@ -1,4 +1,4 @@
-function x=a_msg(x)
+function x=a_msg(x)//1
 x=[0 0 0
  0 0 1
  0 0 2
--- rs_rrscode.m
+++ rs_rrscode.m
@@ -1,3 +1,3 @@
-function new_msg=rs_rrscode(in_msg,all_code_msg,all_msg)
+function new_msg=rs_rrscode(in_msg,all_code_msg,all_msg)//1
 new_msg=rs_interpret(in_msg,all_code_msg,all_msg);
 return;
